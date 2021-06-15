6ba2dd0338a3af883827b0ec40662458d7602877
keith72632 <keithhorton30@outlook.com>
2021-06-15 18:28:47 -0500

    refactor

--- client.py
+++ client.py
@@ -3,31 +3,30 @@ import sys
 from time import sleep
 from pprint import pprint
 
+data = 666
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 KNRM = "\x1B[0m"
 KRED = "\x1B[31m"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as s:
-    s.connect((HOST, PORT))
-    print('Connected to Server')
-    #data = s.recv(69)
-    #print(data)
-    i = 0
-    while 1:
-        print(i)
-        str_i = str(i)
+def sendData(host, port, data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as s:
+        s.connect((host, port))
+        print('Connected to Server')
+        #data = s.recv(69)
+        #print(data)
+        str_i = str(data)
         s.send(str_i.encode())
-        sleep(10)
-        i += 1
-    s.close()
+        print(data)
+        s.close()
 
-addrinfo = socket.getaddrinfo(HOST, PORT)
+def getServerInfo(host, port):
+    addrinfo = socket.getaddrinfo(host, port)
+    print(KRED)
+    print('Server Info')
+    pprint(addrinfo)
+    print(KNRM)
+    print('Host Name:',socket.gethostname())
 
-print(KRED)
-print('Server Info')
-pprint(addrinfo)
-print(KNRM)
-
-print('Host Name:',socket.gethostname())
-
+if __name__ == '__main__':
+    sendData(HOST, PORT, data)
