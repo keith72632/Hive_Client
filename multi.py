b0876156185147c768ec5465777ae3f0718c06a1
keith72632 <keithhorton30@outlook.com>
2021-07-30 07:16:55 -0500

    multithreading server

--- multi.py
+++ multi.py
@@ -1,40 +1,58 @@
 from threading import *
+from datetime import datetime
 import socket
+import sys
 
-SERVER = '192.168.0.183'
-PORT = 666
-
-
-def new1():
-    data = f'From {current_thread().getName()}'
-    data1 = data.encode()
-
+HOST = '192.168.0.183'
+PORT1 = 666
+PORT2 = 888
+def recieving_thread():
+    global DATA
     print(f'Thread: {current_thread().getName()}')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as s:
-        s.connect((SERVER, PORT))
-        print('Connected to server')
+        s.bind((HOST, PORT1))
+        print(f'Listening on address: {HOST} port: {PORT1}')
 
-        s.send(data1)
-        print(f'Data sent: {data1}')
-        s.close()
+        s.listen(5)
 
-def new2():
-    data = f'From {current_thread().getName()}'
-    data1 = data.encode()
+        try:
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    data = conn.recv(1024)
+                    data = data.decode()
+                    if data:
+                        print(f'Data: {data} recieved from {conn.getsockname()} at {datetime.now()}')
+                        DATA = data
+        except KeyboardInterrupt:
+            print('KeyboardInterrupt')
+            s.close()
+            sys.exit()
 
+def sending_thread():
     print(f'Thread: {current_thread().getName()}')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as s:
-        s.connect((SERVER, PORT))
-        print('Connected to server')
+        s.bind((HOST, PORT2))
+        print(f'Listening on address: {HOST} port: {PORT2}')
 
-        s.send(data1)
-        print(f'Data sent: {data1}')
-        s.close()
+        s.listen(5)
 
+        try:
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    data = str(DATA)
+                    data = data.encode()
+                    conn.send(data)
+                    print(f'Data: {data} sent to {conn.getsockname()}')
+        except KeyboardInterrupt:
+            print('Keyboard interrupt')
+            s.close()
+            sys.exit()
 
 #main thread always exectutes
-t1 = Thread(target=new1)
-t2 = Thread(target=new2)
+t1 = Thread(target=recieving_thread)
+t2 = Thread(target=sending_thread)
 t1.start()
 t2.start()
 
